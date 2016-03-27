cdb321f95f14604fe3a56530876d8ee68304dbe5
Jason Brennan <i.jasonbrennan@gmail.com>
2016-03-27 15:46:52 -0400

    Add touch cursors

--- Protos/Scenes/TouchCursorsScene.swift
+++ Protos/Scenes/TouchCursorsScene.swift
@@ -10,4 +10,34 @@ import Prototope
 
 class TouchCursorsScene {
 	
+	var cursorLayers: [UITouchID: Layer] = [:]
+	
+	init() {
+		Layer.root.backgroundColor = Color.white
+		
+		Layer.root.touchBeganHandler = { touch in
+			let cursor = Layer()
+			cursor.backgroundColor = Color.darkGray
+			cursor.size = Size(width: 44, height: 44)
+			cursor.cornerRadius = cursor.size.width / 2.0
+			
+			cursor.position = touch.currentSample.globalLocation
+			
+			self.cursorLayers[touch.id] = cursor
+		}
+		
+		
+		Layer.root.touchMovedHandler = { touch in
+			let cursor = self.cursorLayers[touch.id]!
+			cursor.position = touch.currentSample.globalLocation
+		}
+		
+		
+		Layer.root.touchEndedHandler = { touch in
+			let cursor = self.cursorLayers[touch.id]!
+			cursor.parent = nil
+			
+			self.cursorLayers.removeValueForKey(touch.id)
+		}
+	}
 }
