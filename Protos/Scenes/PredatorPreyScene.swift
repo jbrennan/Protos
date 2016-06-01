98c32783ecca6f950a2599c1082c9e655dbe915d
Jason Brennan <i.jasonbrennan@gmail.com>
2016-06-01 18:38:29 -0400

    Simulation working a bit but wolves never go extinct!

--- Protos/Scenes/PredatorPreyScene.swift
+++ Protos/Scenes/PredatorPreyScene.swift
@@ -11,7 +11,7 @@ import Prototope
 class PredatorPreyScene {
 	
 	var predators: [Predator]
-	let prey: [Entity]
+	var prey: [Entity]
 	
 	var heartBeat: Heartbeat!
 	static let updateInterval = 60
@@ -41,13 +41,94 @@ class PredatorPreyScene {
 					
 					self.predators.append(entity as! Predator)
 				}
+				
+				let livingPrey = self.prey.filter { !$0.isDead }
+				for prey in livingPrey {
+					let entity = prey.copy()
+					entity.position = PredatorPreyScene.randomOnScreenPoint()
+					entity.popIn()
+					
+					self.prey.append(entity)
+				}
 			}
 			
+			// wolves should hunt a bit on each tick:
+			//	- find closet prey
+			//		- if distance is very small, move to it and eat it
+			//		- if distance is decent, move towards the prey
+			//		- else, move to a random spot
+			let livingPredators = self.predators.filter { $0.isDead == false }
+			for predator in livingPredators {
+				let closestPrey = self.nearestPreyToPredator(predator)
+				let distanceToPrey = fabs(predator.position.distanceToPoint(closestPrey.position))
+				
+				let eatingDistance = 5.0
+				let chasingDistance = 50.0
+				
+				if distanceToPrey < eatingDistance {
+					predator.position = closestPrey.position
+					closestPrey.die()
+					predator.hunger = 0
+					
+				} else if distanceToPrey < chasingDistance {
+					predator.position = self.pointAlongLineOfStartingPoint(predator.position, endingPoint: closestPrey.position)
+				} else {
+					predator.position = self.randomPointWithinDistance(2, ofPoint: predator.position)
+				}
+			}
+			
+			
+			// bunnies should just wander, I think
+			//	- maybe eventually they should evade predators
+			let livingPrey = self.prey.filter { !$0.isDead }
+			for prey in livingPrey {
+				prey.position = self.randomPointWithinDistance(2, ofPoint: prey.position)
+			}
 			tickCount += 1
 		})
 	}
 	
+	func nearestPreyToPredator(predator: Predator) -> Entity {
+		var shortestDistance = Double.infinity
+		var closestPrey = prey.first!
+		let livingPrey = prey.filter { !$0.isDead }
+		
+		for bunny in livingPrey {
+			let distanceToPrey = fabs(predator.position.distanceToPoint(bunny.position))
+			
+			if distanceToPrey < shortestDistance {
+				shortestDistance = distanceToPrey
+				closestPrey = bunny
+			}
+		}
+		
+		return closestPrey
+	}
 	
+	/** Returns a `Point` positioned on the line between the given starting and ending points. */
+	func pointAlongLineOfStartingPoint(startingPoint: Point, endingPoint: Point) -> Point {
+		let delta = endingPoint - startingPoint
+		let hypotenuse = sqrt(delta.x * delta.x + delta.y * delta.y)
+		
+		let distanceOnHyp = 5.0 // arbitrary.. should be passed in!
+		
+		let percentageOnHyp = distanceOnHyp / hypotenuse
+		return Point(x: startingPoint.x + percentageOnHyp * delta.x,
+		             y: startingPoint.y + percentageOnHyp * delta.y)
+	}
+	
+	/// Returns a point whose x,y values are randome between the current point and up to `distance` away in any direction
+	func randomPointWithinDistance(distance: Double, ofPoint point: Point) -> Point {
+		// we double the distance, get a random number from 0 < doubleDistance,
+		// then subtract one half (ie distance) from that
+		// so the range becomes -distance < x < distance
+		// finally, add the current x or y to the new random value
+		let doubleDistance = distance * 2
+		let x = (drand48() * doubleDistance) - distance + point.x
+		let y = (drand48() * doubleDistance) - distance + point.y
+		
+		return Point(x: x, y: y)
+	}
 
 }
 
